d5bc0c0e3d4fa3227df85362e0217e21b1092f1e
alexzchen <alexzchen@china-liantong.com>
2012-09-19 23:01:46 +0800

    add ForkTest project

---
+++ ForkTest/src/com/mogoweb/forktest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.mogoweb.forktest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        testFork();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    native void testFork();
+
+    static {
+    	System.loadLibrary("fork_test");
+    }
+}
